12533b9daf0c42177057a6a4de07adec0c2a0add
YunchuWang <yunchuwang5@gmail.com>
2020-12-20 22:31:04 -0600

    move config

--- src/main/java/com/couponbook/ConfigService/ConfigServiceApplication.java
+++ src/main/java/com/couponbook/ConfigService/ConfigServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
+//		System.out.println(System.getProperty("java.class.path"));
 		SpringApplication.run(ConfigServiceApplication.class, args);
 	}
 
